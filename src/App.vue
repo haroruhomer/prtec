b74d5e98e917aa39fe2a118f63692f21a9446896
Harold Molina <harmol22@hotmail.com>
2020-02-20 14:00:05 -0500

    WIP:Sidebar and Header

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,56 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <aside id="nav">
+      <h1>System</h1>
+      <ul>
+        <li><router-link to="/">Users</router-link></li>
+      </ul>
+    </aside>
+    <article id="content">
+      <div id="header-nav">
+        <div id="menu">
+          Dashboard
+        </div>
+        <div id="user" class="">
+          Harold Molina
+        </div>
+      </div>
+      <div class="">
+        <router-view/>        
+      </div>
+    </article>
   </div>
 </template>
 
 <style lang="scss">
+$bgCard : #ffffff;
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  display:flex;
+  background: #f5f6fa;
+  height: 100vh;
 }
 
 #nav {
   padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+  height: 100%;
+  width:20%;
+  background: $bgCard;
+}
+#content{
+  width : 80%;
+  padding: 0 36px;
+}
+#header-nav{
+  background: #ffffff;
+  width: 100%;
+  display: -webkit-box;
+  display: -ms-flexbox;
+  display: flex;
+  justify-content: space-between;
+  min-height: 74px;
+  align-items: center;
+  & div {
+    margin: 0 35px;
   }
 }
 </style>
