bc13dd2a71a4c4b066480bc192d133a217835198
Harold Molina <harmol22@hotmail.com>
2020-02-20 14:58:46 -0500

    WIP:Component Button

---
+++ src/components/BaseButton.vue
@@ -0,0 +1,27 @@
+<template>
+  <button class="btn btn-primary btn-add--user" type="button" name="button">
+    {{msg}}
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'BaseButton',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.btn{
+  border-radius: 8px;
+  padding: 16px;
+  color: #FFFFFF;
+  border: none;
+  &.btn-primary{
+    background: #0054FE;
+  }
+}
+</style>
